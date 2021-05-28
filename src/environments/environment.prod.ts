0e619d8707847e8fde469cff5cc6de3024d10ceb
sirlocust <jua56yu@hotmail.com>
2021-05-27 20:33:20 -0500

    added text to doc and deploy firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCFwicxYQ_SWTWip9Y3LWZAi76Cl0NgaXQ',
+    authDomain: 'technicaltestmontechelo.firebaseapp.com',
+    databaseURL: 'https://technicaltestmontechelo-default-rtdb.firebaseio.com',
+    projectId: 'technicaltestmontechelo',
+    storageBucket: 'technicaltestmontechelo.appspot.com',
+    messagingSenderId: '146120372004',
+    appId: '1:146120372004:web:29ac5ce4ab87ea93ce0db1',
+  },
 };
